b151298def7f25f3ea8259d18b8f41c52cf62ef7
hjuliano24 <hjuliano@hawk.iit.edu>
2024-10-30 13:18:53 -0500

    Last turn in

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function init(){
 //add your javascrip between these two lines of code
- 
-
-
-
-
-
-
+  document.getElementById("entrybutton").addEventListener("click", function() {
+  var entry = document.getElementById("entryinput");
+  document.getElementById("textoutput").innerHTML = entry.value;
+  alert("Hailie Jade Juliano: " + entry.value);
+}, false);
+  
+}
 
 window.addEventListener('load', init);
